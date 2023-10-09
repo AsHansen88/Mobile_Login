d6d3d6024d6c5db3a1db20affb88166c2ae2620f
bobby <ande970s@stud.kea.dk>
2023-10-09 14:07:10 +0200

    :)

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import axios from 'axios'
+import {useState} from 'react'
 
+  
 export default function App() {
+
+  const API_key = "AIzaSyASlK0YEkoiwasDiTYyXBz-HnP5ytIKH3c"
+  const url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=[API_KEY]"
+  const [EnteredEmail, setEnteredEmail] = useState("a@a.dk") 
+  const [EnteredPassword, setEnteredPassword] = useState("123456")
+  
+  async function login(){
+    try{
+      const response = await axios.post(url + API_key, {
+        
+        email: "a@a.dk",
+        password: "123456",
+        returnSecureToken: true
+      
+      })
+      alert("logget ind")
+      
+    }catch(error){
+      alert("Ikke logget ind" + error.response.data.error.errors[0].message)
+    }
+  
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Login</Text>
+      <TextInput 
+      onChangeText={newText => setEnteredEmail(newEmail)}
+      value={EnteredEmail}
+      />
+      <TextInput 
+      onChangeText={newText => setEnteredPassword(newPassword)}
+      value={EnteredPassword}
+      />
+
+      <Button 
+      title='login'
+      onPress={login} 
+      />
+
       <StatusBar style="auto" />
     </View>
   );
